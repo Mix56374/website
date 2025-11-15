060966f275982bd8b58d899d8c13385c9046a93a
Mix56374 <82978103+Mix56374@users.noreply.github.com>
2025-11-15 01:01:29 -0800

    Navbar Updates

--- research/script.js
+++ research/script.js
@@ -1,3 +1,9 @@
 function page(page) {
   document.location=page
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    document.body.classList.remove('loading');
+  }, 10);
+});
