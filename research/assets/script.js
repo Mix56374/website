89d1d12a2bbf6c3b7fc0107fa9d7b7f8c929c06e
Mix56374 <82978103+Mix56374@users.noreply.github.com>
2025-11-16 20:51:08 -0800

    Major Updates

--- research/assets/script.js
+++ research/assets/script.js
@@ -4,13 +4,11 @@ function page(page) {
 
 function toggle_nav_bar() {
   document.getElementById('navbar').classList.toggle('top');
-  document.getElementById('navbutton').classList.toggle('toptoggle');
 }
 
 
 document.addEventListener('DOMContentLoaded', function() {
   var navbar = document.getElementById('navbar')
-  var navbutton = document.getElementById('navbutton')
   var body = document.body
   
   setTimeout(function() {
@@ -20,10 +18,8 @@ document.addEventListener('DOMContentLoaded', function() {
   window.addEventListener('scroll', function() {
     if (window.scrollY <= 0) {
       navbar.classList.remove('top');
-      navbutton.classList.remove('toptoggle');
     } else if (!navbar.classList.contains('top')) {
       navbar.classList.add('top');
-      navbutton.classList.add('toptoggle');
     }
   });
 });
